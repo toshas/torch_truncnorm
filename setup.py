23e4ac0109368c5234fc6d611aa7f90f1c4e401b
anton <anton.obukhov@gmail.com>
2020-10-21 12:52:33 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='torch_truncnorm',
+    version=0.0.1,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description='Truncated Normal distribution in PyTorch',
+    python_requires='>=3.6',
+    packages=find_packages(),
+    author='Anton Obukhov',
+    license='BSD',
+    url='https://www.github.com/toshas/torch_truncnorm',
+)
